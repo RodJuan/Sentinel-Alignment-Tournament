8ba30a150112f202bf16d4b4b42ccd808da31931
Miguel Velasquez <77237593+RodJuan@users.noreply.github.com>
2026-02-27 12:53:20 -0400

    Delete src/ethics_analyzer.py

--- src/ethics_analyzer.py
+++
@@ -1,8 +0,0 @@
-def analyze(results):
-    # Calcula entropía, Gini, bien/mal score basado en collective
-    # Ejemplo simple
-    print("Ethics Analysis: Entropy = X, Gini = Y")
-    # Guarda en JSON para debates AGI
-    with open('ethics_results.json', 'w') as f:
-        import json
-        json.dump({"entropy": 0.5, "gini": 0.3}, f)
