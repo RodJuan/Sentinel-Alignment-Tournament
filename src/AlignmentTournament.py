81bd0a730e83c5aca3a4dff179045ded2698a824
Miguel Velasquez <77237593+RodJuan@users.noreply.github.com>
2026-03-02 00:28:37 -0400

    Update AlignmentTournament.py

--- src/AlignmentTournament.py
+++ src/AlignmentTournament.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import json
-from .strategies import *  # Importa estrategias
 
 class SwarmAgent(nn.Module):
     def __init__(self, id, faction):
